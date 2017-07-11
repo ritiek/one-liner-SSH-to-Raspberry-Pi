6f38350aa4f0d8ebf068a484d74c3dadee8afec2
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2017-07-11 17:35:09 +0530

    Update sshpi.sh

--- sshpi.sh
+++ sshpi.sh
@@ -8,11 +8,8 @@ PI_UID="raspberry_UID"
 # -------------------------------------------------------------
 
 TOKEN="$(curl -sS -X GET -H "content-type:application/json" -H "apikey:WeavedDemoKey\$2015" https://api.weaved.com/v22/api/user/login/$EMAIL/$PASS | cut -d'"' -f 8)"
-
 HOST_IP="$(curl -sS -X GET ipinfo.io/ip/)"
-
 HTTP_HOST="$(curl -sS -X POST -H "content-type:application/json" -H "apikey:WeavedDemoKey\$2015" -H "token:$TOKEN" --data '{"deviceaddress":"'$PI_UID'", "hostip":"$HOST_IP","wait":"true"}' https://api.weaved.com/v22/api/device/connect | cut -d'"' -f 22 | tr -d '\\')"
-
 HTTP_HOST=${HTTP_HOST#"http://"}
 
 HOST=$(echo $HTTP_HOST | cut -d':' -f 1)
